fd9f163455c03eeeb61ae6cfc41970fcf8430f68
DaHu4wA <DaHu4wA@d6005a7f-320d-175c-451a-d015437d1e35>
2013-02-20 16:11:34 +0000

    init

---
+++ trunk/Common/HomeControlCommons/src/at/dahu4wa/homecontrol/model/LogFile.java
@@ -0,0 +1,32 @@
+package at.dahu4wa.homecontrol.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Log file object
+ * 
+ * @author Stefan Huber
+ */
+public class LogFile {
+
+	final List<LogEntry> logEntries;
+
+	public LogFile() {
+		logEntries = new ArrayList<LogEntry>();
+		logEntries.add(new LogEntry("Logfile created"));
+	}
+
+	public List<LogEntry> getLogFile() {
+		return logEntries;
+	}
+
+	public void log(String logEntry) {
+		logEntries.add(new LogEntry(logEntry));
+
+		while (logEntries.size() > 100) {
+			logEntries.remove(0);
+		}
+
+	}
+}
---
+++ trunk/Common/HomeControlCommons/src/at/dahu4wa/homecontrol/model/MediaStatus.java
@@ -0,0 +1,38 @@
+package at.dahu4wa.homecontrol.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class MediaStatus {
+	
+	public final static String STREAM_URL = "stream_url";
+
+	private boolean playing; // if false -> stopped ;)
+	private String information;
+
+	public MediaStatus() {
+		playing = false;
+		information = "no info";
+	}
+
+	public MediaStatus(boolean playing, String information) {
+		this.playing = playing;
+		this.information = information;
+	}
+
+	public boolean isPlaying() {
+		return playing;
+	}
+
+	public void setPlaying(boolean playing) {
+		this.playing = playing;
+	}
+
+	public String getInformation() {
+		return information;
+	}
+
+	public void setInformation(String information) {
+		this.information = information;
+	}
+}
